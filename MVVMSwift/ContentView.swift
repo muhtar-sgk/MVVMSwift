533101eb26cb792f5ffb5e4e0a96b6c7147ea622
muhtar-sgk <muhtarrn@gmail.com>
2023-07-10 18:05:25 +0700

    Initial Commit

---
+++ MVVMSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMSwift
+//
+//  Created by Muhtar on 10/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMSwift/MVVMSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftApp.swift
+//  MVVMSwift
+//
+//  Created by Muhtar on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
